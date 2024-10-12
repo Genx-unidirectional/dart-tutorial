e6d358ac45093ae8f5987afcd582c80d0cc341b5
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-10-12 13:30:51 +0530

    feat basic type in dart

---
+++ 01_basics.dart
@@ -0,0 +1,14 @@
+void main() {
+  print("hello world!");
+  int firstNumber = 23;
+  double secondNumber = 12;
+  print(firstNumber + secondNumber);
+  String myStr = "hello world!";
+  print(myStr);
+  dynamic anyVal = 12;
+  // print(anyVal + "12"); can't do this
+  //this is a comment
+  /*
+  This is multi-line comment
+  */
+}
