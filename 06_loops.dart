0e05ec562dabaf28c89043d64550041853886889
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-10-12 20:35:08 +0530

    feat loops in dart

---
+++ 06_loops.dart
@@ -0,0 +1,16 @@
+void main() {
+  for (int i = 0; i < 10; i++) {
+    print("hello world $i times");
+  }
+  int i = 0;
+  while (i <= 10) {
+    // print("hello");
+    i++;
+  }
+
+  int j = 0;
+  do {
+    print("tim");
+    j++;
+  } while (j < 10);
+}
